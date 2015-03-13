62a9c49cea3725fdaad08680ac5e1a66d226a745
johncodesgood <bs19087@yahoo.com>
2015-03-13 19:43:10 -0400

    adds new survey questions

--- surveynew/surveynew.js
+++ surveynew/surveynew.js
@@ -50,79 +50,44 @@ angular.module('myApp.surveynew', ['ngRoute'])
     };
   };
 
+
   $scope.favoriteAnimalSubmit = function() {
     $scope.favoriteAnimalSubmitted = true;
     if ($scope.userInfo.favoriteAnimal.$valid) {
-      $scope.surveyQuestion = 11;
+      $scope.surveyQuestion = 4;
       $scope.favoriteAnimalSubmitted = false;
     };
   };
 
-  $scope.genderSubmit = function() {
-    $scope.genderSubmitted = true;
-    if ($scope.userInfo.gender.$valid) {
-      $scope.surveyQuestion = 4;
-      $scope.genderSubmitted = false;
-    };
-  };
-
-  $scope.activitiesSubmit = function() {
-    $scope.activitiesSubmitted = true;
-    if ($scope.formInfo.activities) {
+  $scope.homeworkSubmit = function() {
+    $scope.homeworkSubmitted = true;
+    if ($scope.userInfo.homework.$valid) {
       $scope.surveyQuestion = 5;
-      $scope.activitiesSubmitted = false;
+      $scope.homeworkSubmitted = false;
     };
   };
 
-  $scope.topicsSubmit = function() {
-    $scope.topicsSubmitted = true;
-    if ($scope.formInfo.topics) {
+  $scope.favoriteSeasonSubmit = function() {
+    $scope.favoriteSeasonSubmitted = true;
+    if ($scope.userInfo.favoriteSeason.$valid) {
       $scope.surveyQuestion = 6;
-      $scope.topicsSubmitted = false;
+      $scope.favoriteSeasonSubmitted = false;
     };
   };
 
-  $scope.friendsDescribeSubmit = function() {
-    $scope.friendsDescribeSubmitted = true;
-    if ($scope.formInfo.friendsDescribe) {
+  $scope.climbOrRideSubmit = function() {
+    $scope.climbOrRideSubmitted = true;
+    if ($scope.userInfo.climbOrRide.$valid) {
       $scope.surveyQuestion = 7;
-      $scope.friendsDescribeSubmitted = false;
+      $scope.climbOrRideSubmitted = false;
     };
   };
 
-  $scope.extraInfoSubmit = function() {
-    $scope.extraInfoSubmitted = true;
-    if ($scope.userInfo.extraInfo.$valid) {
+  $scope.dogKindSubmit = function() {
+    $scope.dogKindSubmitted = true;
+    if ($scope.userInfo.dogKind.$valid) {
       $scope.surveyQuestion = 8;
-      $scope.extraInfoSubmitted = false;
-    };
-  };
-
-  $scope.friendsTypeSubmit = function() {
-    $scope.friendsTypeSubmitted = true;
-    if ($scope.userInfo.friendsType.$valid) {
-      $scope.surveyQuestion = 9;
-      $scope.friendsTypeSubmitted = false;
-    };
-  };
-
-  $scope.perfectWeekendSubmit = function() {
-    $scope.perfectWeekendSubmitted = true;
-    if ($scope.userInfo.perfectWeekend.$valid) {
-      $scope.surveyQuestion = 10;
-      $scope.perfectWeekendSubmitted = false;
-    };
-  };
-
-  $scope.letsGoSubmit = function() {
-    $scope.letsGoSubmitted = true;
-    if ($scope.userInfo.letsGo.$valid && $scope.formInfo.genderInvite && $scope.formInfo.numPeople) {
-      $scope.letsGoSubmitted = false;
-    if (currentAuth.provider === "password") {
-        $scope.surveyQuestion = 11;
-      } else {
-        writeSurveyData();
-      }
+      $scope.dogKindSubmitted = false;
     };
   };
 
@@ -178,6 +143,10 @@ angular.module('myApp.surveynew', ['ngRoute'])
               firstName: $scope.formInfo.firstName.charAt(0).toUpperCase() + $scope.formInfo.firstName.slice(1),
               age: $scope.formInfo.age,
               favoriteAnimal: $scope.formInfo.favoriteAnimal,
+              homework: $scope.formInfo.homework,
+              favoriteSeason: $scope.formInfo.favoriteSeason,
+              climbOrRide: $scope.formInfo.climbOrRide,
+              dogKind: $scope.formInfo.dogKind,
               dateSignUp: datetime,
               profilePic: $scope.imageUrl || ("https://graph.facebook.com/" + currentAuth.facebook.id +"/picture?type=large&width=200&height=200")
     }; // formData
