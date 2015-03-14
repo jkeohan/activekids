78d575d27ce2cba73965a31950fc0abe411c31e9
johncodesgood <bs19087@yahoo.com>
2015-03-13 22:02:42 -0400

    adds basic data for graph page

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -17,11 +17,19 @@ angular.module('myApp.graphs', ['ngRoute'])
 
 .controller('GraphsCtrl', function($scope, $firebase, FIREBASE_URL, currentAuth) {
    
-   // Hey Joe, all the data for the logged in user is contained in $scope.profile
-   // To access specific pieces of data you would use the syntax $scope.profile.age or $scope.profile.firstName
-   // You'll have to figure out what type of data we want to save for each activity and then
-   // Tamara will add that to each user's profile
    // Take a look at the profile data in the Chrome console from the line below:
    console.log("Profile data:", $scope.profile);
+
+   // With what you're building, we'll probably want to grab all the activity data stored
+   // in a user's profile. Here's how to get that data as an Array
+  // 1. Create a new reference to Firebase database
+  var ref = new Firebase(FIREBASE_URL);
+  // 2. Get the user's activities from firebase in the form of an Array
+  $scope.userActivitiesArray = $firebase(ref.child('users').child(currentAuth.uid).child('activities')).$asArray();
+  // Now you can use ng-repeat on this data in your html file
+  // Log in to the site with the following username and password or go to the "Activities" page
+  // and add some activities to your profile to see this stuff working
+  // username: lizzy@example.com   password: password
+  // Note: there will always be one blank activity in the list - Don't worry about that for now
 
 });
